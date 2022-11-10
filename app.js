314439f74811ae1302aa113cf09aa3c11eb3da8f
Juan Galvez <juangalvezccopa@gmail.com>
2022-11-10 11:20:51 -0500

    jueves

--- app.js
+++ app.js
@@ -1,52 +1,11 @@
 const qrcode = require('qrcode-terminal');
 const { Client, Buttons } = require('whatsapp-web.js');
-const { sendMessage } = require('./controllers/send');
+const { sendMessage, sendMedia } = require('./controllers/send');
 const stepsInitial = require('./messages/initial.json');
 
 // 51958838270@c.us
 const cron = require('node-cron');
 const client = new Client();
-// console.log(stepsInitial);
-const listenMessage = () =>
-  client.on('message', async (msg) => {
-    const { from, body, hasMedia } = msg;
-    if (from == 'status@broadcast' || from == '120363025006115402@g.us') {
-      return;
-    }
-    const date = new Date();
-    const hour = date.getHours();
-    // if (hour < 13) {
-    //   const response = await sendMessage(
-    //     client,
-    //     from,
-    //     'No atendemos a esta hora'
-    //   );
-    //   const { status, message } = response;
-    //   console.log(message);
-    //    return
-    // }
-    if (body == 'a') {
-      // let button = new Buttons(
-      //   'Button body',
-      //   [{ body: 'bt1' }, { body: 'bt2' }, { body: 'bt3' }],
-      //   'title',
-      //   'footer'
-      // );
-      const messages = stepsInitial[0].message;
-      // for (let i = 0; i < messages.length; i++) {
-      //   const response = await sendMessage(client, from, messages[i]);
-      //   const { status, message } = response;
-      //   console.log(message);
-      // }
-      console.log(messages);
-      messages.map(async (e) => {
-        const a = await client.sendMessage(from, e);
-      });
-      // client.sendMessage(from, messages[0]);
-      // client.sendMessage(from, messages[1]);
-      // client.sendMessage(from, messages[2]);
-    }
-  });
 
 client.on('qr', (qr) => {
   qrcode.generate(qr, { small: true });
@@ -54,15 +13,63 @@ client.on('qr', (qr) => {
 
 client.on('ready', () => {
   console.log('client ready');
-  listenMessage();
-  const job = cron
-    .schedule('*/3 * * * * *', function () {
-      console.log('---------------------');
-      client.sendMessage('51991310917@c.us', stepsInitial[0].message[0]);
-      client.sendMessage('51991310917@c.us', stepsInitial[0].message[1]);
-      client.sendMessage('51991310917@c.us', stepsInitial[0].message[2]);
-    })
-    .start();
+  // listenMessage();
+  // const job = cron
+  //   .schedule('*/3 * * * * *', () => {
+  //     console.log('------------------------');
+  //     console.log('Enviando mensaje 1');
+  //     client.sendMessage('51991310917@c.us', stepsInitial[0].message[0]);
+  //     client.sendMessage('51991310917@c.us', stepsInitial[0].message[1]);
+  //     client.sendMessage('51991310917@c.us', stepsInitial[0].message[2]);
+  //   })
+  //   .start();
+});
+client.on('message', (msg) => {
+  const { from, body } = msg;
+  // if (from == 'status@broadcast' || from == '120363025006115402@g.us') {
+  //   return;
+  // }
+  //const date = new Date();
+  //const hour = date.getHours();
+  // if (hour < 13) {
+  //   const response = await sendMessage(
+  //     client,
+  //     from,
+  //     'No atendemos a esta hora'
+  //   );
+  //   const { status, message } = response;
+  //   console.log(message);
+  //    return
+  // }
+  if (body == 'a') {
+    // client.sendMessage(from, 'holaaaaa');
+    //console.log(stepsInitial[0].message.length);
+    // for (let i = 0; i < stepsInitial[0].message.length; i++) {
+    //   sendMessage(client, from, stepsInitial[0].message[i]);
+    // }
+    client.sendMessage(from, 'hola1');
+    client.sendMessage(from, 'hola2');
+    client.sendMessage(from, 'hola3');
+    // sendMedia(client, from, 'Anuncio_bienvenida.png');
+    // // await sendMedia(client, from, 'Anuncio_bienvenida.png');
+    // await sendMessage(client, from, 'hola2');
+    // await sendMessage(client, from, 'hola3');
+    // await sendMedia(client, from, 'Anuncio_bienvenida.png');
+    //const response3 = await sendMedia(client, from, 'Anuncio_bienvenida.png');
+    //const { message } = response3;
+    //console.log(message);
+    // let button = new Buttons(
+    //   'Button body',
+    //   [{ body: 'bt1' }, { body: 'bt2' }, { body: 'bt3' }],
+    //   'title',
+    //   'footer'
+    // );
+    // const messages = stepsInitial[0].message;
+    // console.log(messages);
+    // messages.map(async (e) => {
+    //   const a = await client.sendMessage(from, e);
+    // });
+  }
 });
 
 client.initialize();
--- controllers/send.js
+++ controllers/send.js
@@ -1,11 +1,32 @@
-const sendMessage = async (client, number = null, text = null) => {
-    try {
-        setTimeout(async () => {
-            client.sendMessage(number, text);
-            },170)
-        return {status:true,message:"Mensaje enviado a "+number}
-    } catch (error) {
-        return {status:false,message:"No se envio mensaje a "+number+" error: "+error}
+const fs = require('fs');
+const { MessageMedia } = require('whatsapp-web.js');
+const DIR_MEDIA = `${__dirname}/../media`;
+const sendMessage = (client, number = null, text = null) => {
+  try {
+    client.sendMessage(number, text);
+    console.log('Mensaje enviado a ' + number);
+    return true;
+  } catch (error) {
+    console.log('No se envio mensaje a ' + number + ' error: ' + error);
+    return false;
+  }
+};
+
+const sendMedia = (client, number = null, fileName = null) => {
+  if (!client) return console.error('El objeto cliente no está definido.');
+  try {
+    const file = `${DIR_MEDIA}/${fileName}`;
+    if (fs.existsSync(file)) {
+      const media = MessageMedia.fromFilePath(file);
+      client.sendMessage(number, media, { sendAudioAsVoice: true });
+      console.log('Imagen enviada a ' + number);
+      return true;
     }
-}
-module.exports = { sendMessage}
+    console.log('No se encontró el archivo');
+    return false;
+  } catch (e) {
+    console.log('No se envio imgaen a ' + number + ' error: ' + error);
+    return false;
+  }
+};
+module.exports = { sendMessage, sendMedia };
