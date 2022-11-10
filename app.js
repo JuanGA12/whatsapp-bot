b50f19588b92588bffc7dd2417cbd22c0a4149e4
Juan Galvez <juangalvezccopa@gmail.com>
2022-11-10 18:33:21 -0500

    Pruebas

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 // const fs = require('fs');
 const qrcode = require('qrcode-terminal');
 const { Client, Buttons, LocalAuth } = require('whatsapp-web.js');
-const { sendMessage, sendMedia } = require('./controllers/send');
+const { sendMessage, sendMedia, sendButtons } = require('./controllers/send');
 const stepsInitial = require('./messages/initial.json');
 const cron = require('node-cron');
 
-const numbers = ['51958838270@c.us', '51943731086@c.us', '51991310917@c.us', '51975348795@c.us', '51991692079@c.us'];
+const numbers = [
+  '51959163747@c.us',
+  '51958838270@c.us',
+  '51991310917@c.us',
+  '51975348795@c.us',
+  '51991692079@c.us',
+  '51984112977@c.us',
+  '51941476428@c.us',
+  '51994712454@c.us',
+  '51992386664@c.us',
+];
+const buttons = [
+  { id: 'boton_si', body: 'SI' },
+  { id: 'boton_no', body: 'NO' },
+];
+const emojis = ['😍', '😀', '😐', '🙁', '😠'];
 const client = new Client({
   authStrategy: new LocalAuth({
     clientId: 'client-one',
@@ -19,21 +34,28 @@ client.on('qr', (qr) => {
 client.on('ready', () => {
   console.log('WHATSAPP WEB => Ready');
   const job1 = cron
-    .schedule('0 30 15 * * *', async () => {
+    .schedule('0 10 18 * * *', async () => {
       console.log('--------------------------');
       console.log('Job 1');
       numbers.map(async (number, idx) => {
+        sendMedia(client, number, 'Anuncio_bienvenida.png');
+        await sleep(1000);
         for (let i = 0; i < stepsInitial[0].message.length; i++) {
           sendMessage(client, number, stepsInitial[0].message[i]);
           await sleep(1000);
         }
+        sendButtons(
+          client,
+          number,
+          '¿Te puedo ayudar en algo más?, presiona SI o NO.',
+          buttons
+        );
         await sleep(1000);
-        sendMedia(client, number, 'Anuncio_bienvenida.png');
       });
     })
     .start();
   const job2 = cron
-    .schedule('0 45 15 * * * ', async () => {
+    .schedule('0 13 18 * * * ', async () => {
       console.log('--------------------------');
       console.log('Job 2');
       numbers.map(async (number, idx) => {
@@ -41,25 +63,45 @@ client.on('ready', () => {
           sendMessage(client, number, stepsInitial[1].message[i]);
           await sleep(1000);
         }
+        sendButtons(
+          client,
+          number,
+          '¿Te puedo ayudar en algo más?, presiona SI o NO.',
+          buttons
+        );
+        await sleep(1000);
       });
     })
     .start();
   const job3 = cron
-    .schedule('0 0 16 * * *', async () => {
+    .schedule('0 16 18 * * *', async () => {
       console.log('--------------------------');
       console.log('Job 3');
       numbers.map(async (number, idx) => {
-        for (let i = 0; i < stepsInitial[0].message.length; i++) {
-          sendMessage(client, number, stepsInitial[0].message[i]);
+        for (let i = 0; i < stepsInitial[2].message.length; i++) {
+          sendMessage(client, number, stepsInitial[2].message[i]);
           await sleep(1000);
         }
         await sleep(1000);
-        sendMedia(client, number, 'Anuncio_pagar_servicio.png');
+        sendMedia(
+          client,
+          number,
+          'paso3.png',
+          'Y queremos que te sientas seguro utilizándola. Sigue estas recomendaciones.'
+        );
+        await sleep(1000);
+        sendButtons(
+          client,
+          number,
+          '¿Te puedo ayudar en algo más?, presiona SI o NO.',
+          buttons
+        );
+        await sleep(1000);
       });
     })
     .start();
   const job4 = cron
-    .schedule('0 15 16 * * *', async () => {
+    .schedule('0 19 18 * * *', async () => {
       console.log('--------------------------');
       console.log('Job 4');
       numbers.map(async (number, idx) => {
@@ -67,23 +109,45 @@ client.on('ready', () => {
           sendMessage(client, number, stepsInitial[3].message[i]);
           await sleep(1000);
         }
+
+        sendButtons(
+          client,
+          number,
+          '¿Te puedo ayudar en algo más?, presiona SI o NO.',
+          buttons
+        );
+        await sleep(1000);
       });
     })
     .start();
   const job5 = cron
-    .schedule('0 30 16 * * *', async () => {
+    .schedule('0 21 18 * * *', async () => {
       console.log('--------------------------');
       console.log('Job 5');
       numbers.map(async (number, idx) => {
+        sendMedia(
+          client,
+          number,
+          'Anuncio_pagar_servicio.png',
+          '¡Hola! 👋 ¡Recuerda que ya no es necesario que hagas cola para pagar tus recibos! Ahorra tiempo y recarga tu celular o paga tu luz, agua o teléfono desde la App Interbank.'
+        );
+        await sleep(1000);
         for (let i = 0; i < stepsInitial[4].message.length; i++) {
           sendMessage(client, number, stepsInitial[4].message[i]);
           await sleep(1000);
         }
+        sendButtons(
+          client,
+          number,
+          '¿Te puedo ayudar en algo más?, presiona SI o NO.',
+          buttons
+        );
+        await sleep(1000);
       });
     })
     .start();
   const job6 = cron
-    .schedule('0 45 16 * * *', async () => {
+    .schedule('0 23 18 * * *', async () => {
       console.log('--------------------------');
       console.log('Job 6');
       numbers.map(async (number, idx) => {
@@ -91,28 +155,44 @@ client.on('ready', () => {
           sendMessage(client, number, stepsInitial[5].message[i]);
           await sleep(1000);
         }
+        sendButtons(
+          client,
+          number,
+          '¿Te puedo ayudar en algo más?, presiona SI o NO.',
+          buttons
+        );
+        await sleep(1000);
       });
     })
     .start();
   const job7 = cron
-    .schedule('0 0 17 * * *', async () => {
+    .schedule('0 26 18 * * *', async () => {
       console.log('--------------------------');
       console.log('Job 7');
       numbers.map(async (number, idx) => {
-        for (let i = 0; i < stepsInitial[7].message.length; i++) {
-          sendMessage(client, number, stepsInitial[7].message[i]);
+        for (let i = 0; i < stepsInitial[6].message.length; i++) {
+          sendMessage(client, number, stepsInitial[6].message[i]);
           await sleep(1000);
         }
+        sendButtons(
+          client,
+          number,
+          '¿Te puedo ayudar en algo más?, presiona SI o NO.',
+          buttons
+        );
+        await sleep(1000);
       });
     })
     .start();
   const job8 = cron
-    .schedule('0 15 17 * * *', async () => {
+    .schedule('0 28 18 * * *', async () => {
       console.log('--------------------------');
       console.log('Job 8');
       numbers.map(async (number, idx) => {
-        for (let i = 0; i < stepsInitial[8].message.length; i++) {
-          sendMessage(client, number, stepsInitial[8].message[i]);
+        sendMedia(client, number, 'f.png');
+        await sleep(1000);
+        for (let i = 0; i < stepsInitial[7].message.length; i++) {
+          sendMessage(client, number, stepsInitial[7].message[i]);
           await sleep(1000);
         }
       });
@@ -121,16 +201,33 @@ client.on('ready', () => {
 });
 
 client.on('message', async (msg) => {
-  const { from, body } = msg;
+  const { from, body, selectedButtonId } = msg;
   if (from == 'status@broadcast') {
     return;
   }
-  // for (let i = 0; i < stepsInitial[0].message.length; i++) {
-  //   sendMessage(client, from, stepsInitial[0].message[i]);
-  //   await sleep(1000);
-  // }
-  // await sleep(1000);
-  // sendMedia(client, from, 'Anuncio_bienvenida.png');
+  if (body == 'SI' && selectedButtonId == 'boton_si') {
+    sendMessage(client, from, 'Si tienes alguna duda, escríbenos por aquí 👇');
+    await sleep(1000);
+    sendMessage(
+      client,
+      from,
+      'Recuerda que te atenderemos en nuestro horario de lunes a viernes 9 a 6 pm.'
+    );
+    await sleep(1000);
+  }
+
+  if (body == 'NO' && selectedButtonId == 'boton_no') {
+    sendMessage(client, from, 'Ok, ¡hasta luego! 😊');
+    await sleep(1000);
+  }
+  if ((body >= 1 && body <= 5) || emojis.includes(body)) {
+    sendMessage(
+      client,
+      from,
+      '*¡Excelente!* 🙌  te agradeceríamos que nos dejes un comentario para mejorar poco a poco nuestro contenido.'
+    );
+    await sleep(1000);
+  }
 });
 client.on('authenticated', (session) => {
   console.log('WHATSAPP WEB => Authenticated');
--- controllers/send.js
+++ controllers/send.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { MessageMedia } = require('whatsapp-web.js');
+const { MessageMedia, Buttons } = require('whatsapp-web.js');
 const DIR_MEDIA = `${__dirname}/../media`;
 const sendMessage = (client, number = null, text = null) => {
   try {
@@ -12,13 +12,16 @@ const sendMessage = (client, number = null, text = null) => {
   }
 };
 
-const sendMedia = (client, number = null, fileName = null) => {
+const sendMedia = (client, number = null, fileName = null, caption = null) => {
   if (!client) return console.error('El objeto cliente no está definido.');
   try {
     const file = `${DIR_MEDIA}/${fileName}`;
     if (fs.existsSync(file)) {
       const media = MessageMedia.fromFilePath(file);
-      client.sendMessage(number, media, { sendAudioAsVoice: true });
+      client.sendMessage(number, media, {
+        sendAudioAsVoice: true,
+        caption: caption ? caption : '',
+      });
       console.log('Imagen enviada a ' + number);
       return true;
     }
@@ -29,4 +32,22 @@ const sendMedia = (client, number = null, fileName = null) => {
     return false;
   }
 };
-module.exports = { sendMessage, sendMedia };
+const sendButtons = (
+  client,
+  number,
+  message = null,
+  buttons,
+  title = null,
+  footer = null
+) => {
+  try {
+    let button = new Buttons(message, buttons, title, footer);
+    client.sendMessage(number, button);
+    console.log('Botones enviados a ' + number);
+    return true;
+  } catch (error) {
+    console.log('No se envio botones a ' + number + ' error: ' + error);
+    return false;
+  }
+};
+module.exports = { sendMessage, sendMedia, sendButtons };
